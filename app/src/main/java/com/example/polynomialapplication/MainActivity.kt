b778106e13b6c16a2a5edd99c1d775d2d9d44406
lechos22 <mazurlech8@gmail.com>
2023-03-27 21:50:25 +0200

    Added scaffold with a top app bar

--- app/src/main/java/com/example/polynomialapplication/MainActivity.kt
+++ app/src/main/java/com/example/polynomialapplication/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.polynomialapplication
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.ComponentActivity
@@ -20,117 +21,122 @@ class MainActivity : ComponentActivity() {
         System.err.println(e.message.toString())
     }
     @OptIn(ExperimentalLayoutApi::class)
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContent {
-            PolynomialApplicationTheme {
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    var polynomialList by rememberSaveable(
-                        stateSaver = PolynomialListSaver(),
-                        init = { mutableStateOf(listOf()) }
+    @Composable
+    fun Calculator(modifier: (Modifier) -> Modifier){
+        var polynomialList by rememberSaveable(
+            stateSaver = PolynomialListSaver(),
+            init = { mutableStateOf(listOf()) }
+        )
+        FlowColumn(
+            modifier = Modifier.padding(10.dp).run(modifier)
+        ) {
+            FlowRow(
+                horizontalArrangement = Arrangement.SpaceBetween,
+                modifier = Modifier
+                    .fillMaxWidth(1F)
+                    .fillMaxHeight(0.2F)
+            ) {
+                PolynomialInput {
+                    polynomialList = polynomialList + listOf(
+                        if (it.trim() == "")
+                            Polynomial()
+                        else
+                            Polynomial.fromString(it)
                     )
-                    FlowColumn(
-                        modifier = Modifier.padding(10.dp)
-                    ) {
-                        FlowRow(
-                            horizontalArrangement = Arrangement.SpaceBetween,
-                            modifier = Modifier
-                                .fillMaxWidth(1F)
-                                .fillMaxHeight(0.2F)
-                        ) {
-                            PolynomialInput {
-                                polynomialList = polynomialList + listOf(
-                                    if (it.trim() == "")
-                                        Polynomial()
-                                    else
-                                        Polynomial.fromString(it)
-                                )
-                            }
+                }
+            }
+            FlowRow(
+                horizontalArrangement = Arrangement.SpaceBetween,
+                modifier = Modifier
+                    .fillMaxWidth(1F)
+                    .fillMaxHeight(0.8F)
+                    .padding(5.dp)
+            ) {
+                if(polynomialList.isNotEmpty()) {
+                    LazyColumn(
+                        modifier = Modifier
+                            .fillMaxHeight()
+                            .fillMaxWidth(0.5F)
+                    ){
+                        itemsIndexed(polynomialList) { _, item ->
+                            PolynomialView(item)
                         }
-                        FlowRow(
-                            horizontalArrangement = Arrangement.SpaceBetween,
-                            modifier = Modifier
-                                .fillMaxWidth(1F)
-                                .fillMaxHeight(0.8F)
-                                .padding(5.dp)
-                        ) {
-                            if(polynomialList.isNotEmpty()) {
-                                LazyColumn(
-                                    modifier = Modifier
-                                        .fillMaxHeight()
-                                        .fillMaxWidth(0.5F)
-                                ){
-                                    itemsIndexed(polynomialList) { _, item ->
-                                        PolynomialView(item)
-                                    }
+                    }
+                    FlowColumn(Modifier.fillMaxWidth(0.5F)) {
+                        val size = polynomialList.size
+                        MyButton(icon = null, text = "Pop") {
+                            polynomialList = polynomialList.subList(0, size - 1)
+                        }
+                        MyButton(icon = null, text = "Clone") {
+                            polynomialList = polynomialList + polynomialList.last()
+                        }
+                        MyButton(icon = null, text = "Factorize") {
+                            polynomialList = polynomialList.subList(0, size - 1) +
+                                polynomialList.last().factorize()
+                        }
+                        if(size >= 2) {
+                            MyButton(icon = null, text = "Swap") {
+                                polynomialList = polynomialList.subList(0, size - 2) +
+                                    listOf(
+                                        polynomialList[size - 1],
+                                        polynomialList[size - 2]
+                                    )
+                            }
+                            MyButton(icon = null, text = "Add") {
+                                polynomialList = polynomialList.subList(0, size - 2) +
+                                    (polynomialList[size - 2] + polynomialList[size - 1])
+                            }
+                            MyButton(icon = null, text = "Subtract") {
+                                polynomialList = polynomialList.subList(0, size - 2) +
+                                    (polynomialList[size - 2] - polynomialList[size - 1])
+                            }
+                            MyButton(icon = null, text = "Multiply") {
+                                polynomialList = polynomialList.subList(0, size - 2) +
+                                    (polynomialList[size - 2] * polynomialList[size - 1])
+                            }
+                            MyButton(icon = null, text = "Divide") {
+                                try {
+                                    polynomialList = polynomialList.subList(0, size - 2) +
+                                        (polynomialList[size - 2] / polynomialList[size - 1])
+                                } catch (e: ArithmeticException) {
+                                    onArithmeticException(e)
                                 }
-                                FlowColumn(Modifier.fillMaxWidth(0.5F)) {
-                                    val size = polynomialList.size
-                                    MyButton(icon = null, text = "Pop") {
-                                        polynomialList = polynomialList.subList(0, size - 1)
-                                    }
-                                    MyButton(icon = null, text = "Clone") {
-                                        polynomialList = polynomialList + polynomialList.last()
-                                    }
-                                    MyButton(icon = null, text = "Factorize") {
-                                        polynomialList = polynomialList.subList(0, size - 1) +
-                                            polynomialList.last().factorize()
-                                    }
-                                    if(size >= 2) {
-                                        MyButton(icon = null, text = "Swap") {
-                                            polynomialList = polynomialList.subList(0, size - 2) +
-                                                listOf(
-                                                    polynomialList[size - 1],
-                                                    polynomialList[size - 2]
-                                                )
-                                        }
-                                        MyButton(icon = null, text = "Add") {
-                                            polynomialList = polynomialList.subList(0, size - 2) +
-                                                (polynomialList[size - 2] + polynomialList[size - 1])
-                                        }
-                                        MyButton(icon = null, text = "Subtract") {
-                                            polynomialList = polynomialList.subList(0, size - 2) +
-                                                (polynomialList[size - 2] - polynomialList[size - 1])
-                                        }
-                                        MyButton(icon = null, text = "Multiply") {
-                                            polynomialList = polynomialList.subList(0, size - 2) +
-                                                (polynomialList[size - 2] * polynomialList[size - 1])
-                                        }
-                                        MyButton(icon = null, text = "Divide") {
-                                            try {
-                                                polynomialList = polynomialList.subList(0, size - 2) +
-                                                    (polynomialList[size - 2] / polynomialList[size - 1])
-                                            } catch (e: ArithmeticException) {
-                                                onArithmeticException(e)
-                                            }
-                                        }
-                                        MyButton(icon = null, text = "Remainder") {
-                                            try {
-                                                polynomialList = polynomialList.subList(0, size - 2) +
-                                                    (polynomialList[size - 2] % polynomialList[size - 1])
-                                            } catch (e: ArithmeticException) {
-                                                onArithmeticException(e)
-                                            }
-                                        }
-                                        MyButton(icon = null, text = "Divide with remainder") {
-                                            try {
-                                                polynomialList =
-                                                    polynomialList.subList(0, size - 2) +
-                                                        polynomialList[size - 2]
-                                                            .divRem(polynomialList[size - 1])
-                                                            .toList()
-                                            } catch (e: java.lang.ArithmeticException) {
-                                                onArithmeticException(e)
-                                            }
-                                        }
-                                    }
+                            }
+                            MyButton(icon = null, text = "Remainder") {
+                                try {
+                                    polynomialList = polynomialList.subList(0, size - 2) +
+                                        (polynomialList[size - 2] % polynomialList[size - 1])
+                                } catch (e: ArithmeticException) {
+                                    onArithmeticException(e)
+                                }
+                            }
+                            MyButton(icon = null, text = "Divide with remainder") {
+                                try {
+                                    polynomialList =
+                                        polynomialList.subList(0, size - 2) +
+                                            polynomialList[size - 2]
+                                                .divRem(polynomialList[size - 1])
+                                                .toList()
+                                } catch (e: java.lang.ArithmeticException) {
+                                    onArithmeticException(e)
                                 }
                             }
                         }
                     }
+                }
+            }
+        }
+    }
+    @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+    @OptIn(ExperimentalMaterial3Api::class)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            PolynomialApplicationTheme {
+                Scaffold(topBar = { CenterAlignedTopAppBar(title = {
+                    Text("Polynomial calculator")
+                }) }) {
+                    Calculator { it.padding(top = 50.dp) }
                 }
             }
         }
