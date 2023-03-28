005e38702e7aeba6070b55238f9915aff3092ce3
lechos22 <mazurlech8@gmail.com>
2023-03-28 20:20:32 +0200

    updating RealProduct

--- app/src/main/java/com/lechos22/polynomialapplication/real/RealProduct.kt
+++ app/src/main/java/com/lechos22/polynomialapplication/real/RealProduct.kt
@@ -2,6 +2,6 @@ package com.lechos22.polynomialapplication.real
 
 class RealProduct(vararg xs: Real) : Real {
     val xs: List<Real> = xs.flatMap { if(it is RealProduct) it.xs else listOf(it) }
-    override fun invoke() = xs.fold(1.0) { prev, next -> prev * next() }
+    override fun invoke() = xs.map(Real::invoke).fold(1.0, Double::times)
     override fun toString() = "(* ${xs.joinToString(" ")})"
 }
